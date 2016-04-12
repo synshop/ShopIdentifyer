0ef8c4dc2b65f8f740169937ba4a3994ad00afa5
Brian Munroe <brian.e.munroe@gmail.com>
2016-04-12 07:18:09 -0700

    almost have SVG badge output working.  Dimensions need some tweaking

--- identity/__init__.py
+++ identity/__init__.py
@@ -12,6 +12,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 import svgwrite, json
 from svgwrite.image import Image
+from svgwrite.shapes import Rect
 import MySQLdb as mysql
 
 from flask import Flask, request, g, flash
@@ -515,16 +516,26 @@ def member_vetted(stripe_id):
 @app.route('/member/<stripe_id>/files/badge.svg',methods=['GET'])
 def member_badge(stripe_id):
 
-    svg_document = svgwrite.Drawing(size = ("3.375in", "2.125in"))
+    # http://pixelyzer.com/inches_to_pixels.html
 
-    logo = Image(href="/static/images/syn_shop_badge_logo.png", insert=("0.5in",'0.5in'), size=("1in","1in"))
+    member = get_member(stripe_id)
+
+    svg_document = svgwrite.Drawing(size = ("2.125in","3.375in"))
+
+    border = Rect(insert=(0,0),size=("2.125in","3.375in"),fill="#eeeeee")
+    svg_document.add(border)
+
+    #logo_text = svg_document.text("SYN Shop",insert = ("1in",".25in"),style="font-family:helvetica;font-weight:bold;font-size:16pt;")
+    #svg_document.add(logo_text)
+
+    logo = Image(href="/static/images/syn_shop_badge_logo3.png", insert=("0.3125in",'0.0625in'), size=("1.5in","1.5in"))
     svg_document.add(logo)
 
     photo_url = "/member/%s/files/photo.jpg" % (stripe_id,)
-    photo = Image(href=photo_url,insert=("1.5in",'1.5in'),size=("1in","1in"))
+    photo = Image(href=photo_url,insert=("0.0625in",'1.38in'),size=("2in","2in"))
     svg_document.add(photo)
 
-    #svg_document.add(svg_document.text("Hello World",insert = ("1in","1in")))
+    svg_document.add(svg_document.text(member['full_name'],insert = ("0.0625in","3.300in"),style="font-family:helvetica;font-weight:bold;"))
 
     response = make_response(svg_document.tostring())
     response.headers['Content-Description'] = 'Badge'
@@ -666,7 +677,7 @@ def admin():
 @app.route('/admin/onboard')
 def admin_onboard():
     try:
-        if session['logged_in']:
+        if session.get('logged_in'):
 
             entries_x = []
 
@@ -694,11 +705,11 @@ def admin_onboard():
 
 @app.route('/admin/badgephoto')
 def admin_badge_photo():
-    
-    if session['logged_in']:
+
+    if session.get('logged_in'):
         return render_template('badge_photo.html')
     else:
-        return redirect('/login?redirect_to=admin_badge_photo')
+        return render_template('login.html',r_to=request.referrer)
 
 # Badge Swipe API Endpoint
 @app.route('/swipe/', methods=['POST'])
