079bb3535b2eab8263f5d4202d051abb23689d8d
Ashley <8253488+mrjones-plip@users.noreply.github.com>
2022-11-25 10:23:52 -0800

    fix crypt command, add more error handling  (#54)
    * fix crypt command, add more error handling per #53
    
    * correct args, update readme with same per feedback

--- identity/crypto/crypt.py
+++ identity/crypto/crypt.py
@@ -5,30 +5,56 @@ import sys, getpass
 import CryptoUtil
 from CryptoUtil import KeyLengthError
 
-def usage():
-    exit()
 
 def main(args):
+
+    if len(args) > 1 or args[0] == './identity/crypto/crypt.py':
+        print('Do not call this directly, use symlinks. See https://github.com/synshop/ShopIdentifyer')
+        quit()
 
     op = args[0]
 
     if 'encrypt' in op:
         key = getpass.getpass('Please enter the encryption key: ')
         plaintext = getpass.getpass('Please enter the plaintext you wish to encrypt: ')
-        try:
-            encrypted_value = CryptoUtil.encrypt(plaintext, key)
-            print("Encrypted Value: " + encrypted_value)
-        except KeyLengthError as ex:
-            print (ex)
+        if len(key) > 0 or len(plaintext) > 0:
+            try:
+                encrypted_value = CryptoUtil.encrypt(plaintext, key)
+                print("Encrypted Value: " + encrypted_value)
+            except KeyLengthError as ex:
+                print('ERROR', 'Key Length Error')
+                print(ex)
+            except Exception as e:
+                print('ERROR', 'Error in encrypting. Please check in inputs and try again.')
+                print(e)
+        else:
+            print('Key and plaintext must be at least 1 character long')
+
+        quit()
 
     if 'decrypt' in op:
-        key = getpass.getpass('Please enter the encryption key: ')
-        encrypted_value = getpass.getpass('Please enter the encrypted string you wish to decrypt: ')
-        try:
-            decrypted_value = CryptoUtil.decrypt(encrypted_value, key)
-            print("Plaintext Value: " + decrypted_value)
-        except KeyLengthError as ex:
-            print(ex)
+        key = getpass.getpass('Please enter the decryption key: ')
+        ciphertext = getpass.getpass('Please enter the ciphertext you wish to decrypt: ')
+
+        if len(key) > 0 or len(ciphertext) > 0:
+            try:
+                decrypted_value = CryptoUtil.decrypt(ciphertext, key)
+                print("Plaintext Value: " + decrypted_value)
+            except KeyLengthError as ex:
+                print('ERROR', 'Key Length Error')
+                print(ex)
+            except Exception as e:
+                print('ERROR', 'Error in decrypting. Please check in inputs and try again.')
+                print(e)
+
+        else:
+            print('Key and ciphertext must be at least 1 character long')
+
+        quit()
+
+    else:
+        print('Valid arguments: "encrypt" or "decrypt". See https://github.com/synshop/ShopIdentifyer')
+
 
 if __name__ == "__main__":
     main(sys.argv)
